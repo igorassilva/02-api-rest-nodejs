ae700dd1e884017c76c6a7f9ae0fa70e71889c28
Igor Augusto <igor.a.s.silva@gmail.com>
2023-05-14 20:52:23 -0300

    chore: add root endpoint to health checks

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ import { transactionsRoutes } from './routes/transactions'
 
 export const app = fastify()
 
+app.get('/', async (request, reply) => {
+  return reply.status(200).send()
+})
+
 app.register(cookie)
 app.register(transactionsRoutes, {
   prefix: 'transactions',
